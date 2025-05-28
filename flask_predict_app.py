2fc5538896428e11bc563a8845b94256afb5c792
thnbih <22520136@gm.uit.edu.vn>
2025-05-28 18:48:56 +0700

    add port

--- flask_predict_app.py
+++ flask_predict_app.py
@@ -219,4 +219,7 @@ def upload_file():
 if __name__ == '__main__':
     import urllib3
     urllib3.disable_warnings()
-    app.run(debug=True)
+
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
